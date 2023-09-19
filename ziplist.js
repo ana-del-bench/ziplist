a58ec99ccb94a146f5af486e87da876ae7fa658b
ana-del-bench <delbanco21@gmail.com>
2023-09-18 21:30:53 -1000

    finished in 13m 53.89s

---
+++ ziplist.js
@@ -0,0 +1,18 @@
+const test1 = [1, 2, 3, 4];
+const test2 = ['a', 'b', 'c', 'd'];
+
+function zipList(list1, list2) {
+  const newList = [];
+  for (let i = 0; i < list1.length; i++) {
+    newList.push(list1[i]);
+    newList.push(list2[i]);
+  }
+  return newList;
+}
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.zip(list1, list2);
+}
+
+console.log(zipList(test1, test2));
+console.log(zipListTheSimpleWay(test1, test2));
